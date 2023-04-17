bcb6f3ff64ee94a777b2b43eb40e9b147f536cf8
root <root@NINF004MOCS00.grupoconexao.com.br>
2023-04-17 14:57:18 -0300

    firt commit

---
+++ ex003/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php 
+        $name = "Bruno";
+        $lastName = "Mello";
+
+        const COUNTRY = "Brasil";
+
+        echo "Muito prazer, $name $lastName! Você mora no " . COUNTRY
+    ?>
+</body>
+
+</html>
